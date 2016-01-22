130ba781939dc21c5d979fea57adfc319a1eccc8
Stefan Schindler <stefan@boxbox.org>
2016-01-22 23:32:42 +0100

    Added bar, cherries, adjusted drums. All picture textures get added to each drum automatically.

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,27 @@
 
 class Drum : public sf::Drawable, public sf::Transformable {
   public:
-    Drum(
-      const std::vector<const sf::Texture*>& textures,
-      std::size_t picture_count
-    ) :
+    Drum(const std::vector<const sf::Texture*>& textures) :
       sf::Drawable(),
       _textures(textures),
-      _pictures(picture_count)
+      _pictures(textures.size())
     {
       assert(textures.size() > 0);
-
-      const sf::Texture& first_texture = *textures.front();
 
       for(
         std::size_t picture_idx = 0;
         picture_idx < _pictures.size();
         ++picture_idx
       ) {
+        const sf::Texture& texture = *textures[picture_idx];
+
         sf::Sprite& picture = _pictures[picture_idx];
         picture.setPosition({
             0.0f,
-            static_cast<float>(picture_idx) * first_texture.getSize().y
+            static_cast<float>(picture_idx) * texture.getSize().y
         });
 
-        picture.setTexture(first_texture, true);
+        picture.setTexture(texture, true);
       }
     }
 
@@ -50,7 +47,6 @@ class Drum : public sf::Drawable, public sf::Transformable {
 };
 
 const sf::VideoMode VIDEO_MODE(1280, 720);
-const std::size_t PICTURE_COUNT_PER_DRUM = 3;
 
 int main() {
   sf::RenderWindow window(VIDEO_MODE, "SFML", sf::Style::Close);
@@ -60,10 +56,14 @@ int main() {
   sf::Texture drums_texture;
   sf::Texture apple_texture;
   sf::Texture coin_texture;
+  sf::Texture bar_texture;
+  sf::Texture cherries_texture;
 
   drums_texture.loadFromFile(ASSETS_PATH + "/drums.png");
   apple_texture.loadFromFile(ASSETS_PATH + "/apple.png");
   coin_texture.loadFromFile(ASSETS_PATH + "/coin.png");
+  bar_texture.loadFromFile(ASSETS_PATH + "/bar.png");
+  cherries_texture.loadFromFile(ASSETS_PATH + "/cherries.png");
 
   // Sprites.
   sf::Sprite drums_sprite(drums_texture);
@@ -80,33 +80,36 @@ int main() {
   std::vector<const sf::Texture*> picture_textures;
   picture_textures.push_back(&apple_texture);
   picture_textures.push_back(&coin_texture);
+  picture_textures.push_back(&bar_texture);
+  picture_textures.push_back(&cherries_texture);
 
   // Drums.
   std::vector<Drum> drums;
-  drums.emplace_back(picture_textures, PICTURE_COUNT_PER_DRUM);
-  drums.emplace_back(picture_textures, PICTURE_COUNT_PER_DRUM);
-  drums.emplace_back(picture_textures, PICTURE_COUNT_PER_DRUM);
+  drums.emplace_back(picture_textures);
+  drums.emplace_back(picture_textures);
+  drums.emplace_back(picture_textures);
 
   {
     float drum_width =
       static_cast<float>(drums_texture.getSize().x / drums.size());
     sf::Vector2f drum_center(
-      drums_texture.getSize().x / drums.size() / 2.0f,
-      drums_texture.getSize().y / 2.0f
+      drum_width / 2.0f, drums_texture.getSize().y / 2.0f
     );
+    // lol just guessing right now.. Almost correct though! ;-)
+    sf::Vector2f drum_position =
+      drums_sprite.getPosition() -
+      sf::Vector2f(
+        drums_sprite.getOrigin().x / 2.0f + drum_center.x / 2.0f, 0.0f
+      )
+    ;
 
     for(std::size_t drum_idx = 0; drum_idx < drums.size(); ++drum_idx) {
       Drum& drum = drums[drum_idx];
 
       drum.setOrigin(drum_center);
+      drum.setPosition(drum_position);
 
-      sf::Vector2f position(
-        static_cast<float>(window.getSize().x) / 2.0f,
-        static_cast<float>(window.getSize().y) / 2.0f
-      );
-      position.x += (drum_idx * drum_width);
-
-      drum.setPosition(position);
+      drum_position.x += drum_width;
     }
   }
 
